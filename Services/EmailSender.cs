418aa96ba1717654080e789be140b1038e139493
nono83 <arnaud.gianati@club-internet.fr>
2024-10-25 18:53:59 +0200

    fix(mailing) : Move sendGridKey value from secrets.json to appsettings.json

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -9,23 +9,28 @@ namespace BilleterieParis2024.Services
     public class EmailSender : IEmailSender
     {
         private readonly ILogger _logger;
+        private readonly IConfiguration _config;
 
         public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
-                           ILogger<EmailSender> logger)
+                           ILogger<EmailSender> logger,IConfiguration config)
         {
             Options = optionsAccessor.Value;
             _logger = logger;
+            _config = config;
         }
 
         public AuthMessageSenderOptions Options { get; } //Set with Secret Manager.
 
         public async Task SendEmailAsync(string toEmail, string subject, string message)
         {
-            if (string.IsNullOrEmpty(Options.SendGridKey))
+            string apiSendGridKey = _config["SendGridKey"];
+            //if (string.IsNullOrEmpty(Options.SendGridKey))
+            if (string.IsNullOrEmpty(apiSendGridKey))
             {
                 throw new Exception("Null SendGridKey");
             }
-            await Execute(Options.SendGridKey, subject, message, toEmail);
+            //await Execute(Options.SendGridKey, subject, message, toEmail);
+            await Execute(apiSendGridKey, subject, message, toEmail);
         }
 
         public async Task Execute(string apiKey, string subject, string message, string toEmail)
